ca48bfd868444448260ab4df279acaef562b1979
ramu singh <rulimorees@gmail.com>
2024-07-31 17:59:11 +0530

    'commit'

--- GD/src/COMPONENTS/Label.jsx
+++ GD/src/COMPONENTS/Label.jsx
@@ -3,12 +3,11 @@ import React from "react";
 const Label = ({ fields, children }) => {
   return (
     <div className="flex flex-col md:flex-row gap-4">
-      <label className=" bg-indigo-200 pl-4 pt-2 " htmlFor={fields.label} style={{minWidth: '200px'}} >
-        {fields.label}
+      <label className=" bg-indigo-100 px-4 pt-2 " htmlFor={fields.label} style={{minWidth: '200px'}} >
+        {fields.label} <span className="text-red-700">{fields.required ? "*" : null}</span> 
       </label>
       {children}
     </div>
   );
 };
-
 export default Label;
